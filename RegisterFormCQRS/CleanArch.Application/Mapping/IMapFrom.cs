f6bb38a62c8a726df24cc363b8c6513ed90764d8
Karim_Alaa <karim.alaa@dbsmena>
2021-01-07 09:14:07 +0200

    Add auto mapper in application layer to map the dto to domain entities add model folder to map the response result

---
+++ RegisterFormCQRS/CleanArch.Application/Mapping/IMapFrom.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.Mapping
+{
+    public interface IMapFrom<T>
+    {
+        public void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
+    }
+}
---
+++ RegisterFormCQRS/CleanArch.Application/Models/ResponseResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CleanArch.Application.Models
+{
+    public class ResponseResult
+    {
+        public bool Succeeded { get; set; }
+        public string[] Errors { get; set; }
+
+        public string Msg { set; get; }
+
+        public ResponseResult() { }
+
+        public ResponseResult(bool succeeded, IEnumerable<string> errors, string Msg)
+        {
+            Succeeded = succeeded;
+            Errors = errors.ToArray();
+            this.Msg = Msg;
+        }
+
+
+        public static ResponseResult Success(string Msg)
+        {
+            return new ResponseResult(true, new string[] { }, Msg);
+        }
+
+        public static ResponseResult Failure(IEnumerable<string> errors, string Msg)
+        {
+            return new ResponseResult(false, errors, Msg);
+        }
+    }
+}
