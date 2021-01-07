cecf8848f824b84b2d0492a54bd755de1b529bc3
Karim_Alaa <karim.alaa@dbsmena>
2021-01-07 09:22:50 +0200

    Add MediatR Package Add Generic interface ICommand to map the command requests Add ICommandHandler to handle the request

---
+++ RegisterFormCQRS/CleanArch.Application/Common/Command/ICommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.Common.Command
+{
+    public interface ICommand : IRequest
+    {
+
+    }
+
+    public interface ICommand<out TResult> : IRequest<TResult>
+    {
+    }
+}
---
+++ RegisterFormCQRS/CleanArch.Application/Common/Command/ICommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Application.Common.Command
+{
+    public interface ICommandHandler<in TCommand> : IRequestHandler<TCommand> where TCommand : ICommand
+    {
+
+    }
+
+    public interface ICommandHandler<in TCommand, TResult> :
+    IRequestHandler<TCommand, TResult> where TCommand : ICommand<TResult>
+    {
+    }
+}
